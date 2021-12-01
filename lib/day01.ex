6fde996de560d96782c8b866033daaac93072b87
kemal yucetin <kemalyucetin@gmail.com>
2021-12-01 21:10:25 +0000

    second part

--- lib/day01.ex
+++ lib/day01.ex
@@ -4,7 +4,18 @@ defmodule Day01 do
   def run([first | second]) when hd(second) > first, do: 1 + run(second)
   def run([first | second]), do: run(second)
 
-  def input() do
+  def run2() do
+    calc2(input())
+    |> run()
+  end
+
+  def calc2([first | [second | [third | _tail]]] = list) do
+    [first + second + third] ++ calc2(tl(list))
+  end
+
+  def calc2([_, _]), do: []
+
+  defp input() do
     "lib/inputs/01.input"
     |> File.read!()
     |> String.trim()
